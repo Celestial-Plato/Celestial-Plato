f15aad5e48caa3ea1670e6b175e3645ce40a3b90
Celestial <1193317301@qq.com>
2021-07-12 16:38:49 +0800

    lodash

--- miao/lodash/celestial_plato-lodash.js
+++ miao/lodash/celestial_plato-lodash.js
@@ -45,12 +45,70 @@ var celestial_plato = function () {
         return result;
     }
 
+    function fill(a, str, start = 0, end = a.length) {
+        for(var i = start; i < end; i++) {
+            a[i] = str;
+        }
+        return str;
+    }
+
+    function iterator(it) {
+        if (typeof it === "string") {
+            return val => val[it];
+        }
+        if(typeof it=== "function") {
+            return it;
+        }
+    }
+
+    function zip(...args) {
+        var result = [], arr= [].concat(...args), a = [], b = [];
+        for(var i = 0; i < arr.length; i++) {
+            i % 2 === 0 ? a.push(arr[i]) : b.push(arr[i]);
+        }
+        result.push(a);
+        result.push(b);
+        return result;
+    }
+    function unzip(array) {
+        var result = [];
+        for(var i = 0; i < array[0].length; i++) {
+            var arr = [];
+            arr.push(array[0][i],array[1][i]);
+            result.push(arr);
+        }
+        return result;
+    }
+
+    function uniq(array) {
+        var arr = [];
+        for(let val of array) {
+            if(!arr.includes(val)) arr.push(val);
+        }
+        return arr;
+    }
+
+    function uniqBy(array, f) {
+        var it = iterator(f), judge = [], arr = [];
+        for(let val of array) {
+            if(!judge.includes(it(val))) {
+                arr.push(val);
+                judge.push(it(val));
+            }
+        }
+        return arr;
+    }
+
 
     return {
         chunk: chunk,
         compact: compact,
         difference: difference,
         drop: drop,
-        dropRight: dropRight
+        dropRight: dropRight,
+        uniq: uniq,
+        uniqBy: uniqBy,
+        zip: zip,
+        unzip: unzip
     }
 }();
