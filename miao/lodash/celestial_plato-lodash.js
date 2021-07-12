3730e9010fd65ade01cfe9801c19718123bfc920
Celestial <1193317301@qq.com>
2021-07-12 13:29:46 +0800

    lodash

---
+++ miao/lodash/celestial_plato-lodash.js
@@ -0,0 +1,37 @@
+var celestial_plato = function () {
+
+    function chunk(a, n) {
+        var result = [], count = 0, arr = [];
+        for(var i = 0; i < a.length; i++) {//定义一个数组；
+            arr.push(a[i]);
+            count++;
+            if (count === n) {
+                count = 0;
+                result.push(arr);
+                arr = [];
+            }
+        }
+        result.push(arr);
+        return result
+    }
+
+    function compact(array) {
+        return array.filter(i => !!i === true);
+    }
+
+    function difference(a,...arg) {
+        var arr = concat(...arg)
+        for(var i=0; i<a.length; i++) {
+            if (!arr.includes(a[i])) {
+                result.push(a[i]);
+            }
+        }
+        return result;
+
+    }
+    return {
+        chunk: chunk,
+        compact: compact,
+        difference: difference
+    }
+}();
